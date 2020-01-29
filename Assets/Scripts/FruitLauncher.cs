4f568fb6fd0bd948a6d453f8c790644c5f7c4268
John Hay Supetran <superjohnhay@gmail.com>
2020-01-29 18:12:19 +0800

    First commit

---
+++ Assets/Scripts/FruitLauncher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitLauncher : MonoBehaviour
+{
+	public GameObject fruit;
+	public float minInterval;
+	public float maxInterval;
+	public float startTime;
+	
+	
+    // Start is called before the first frame update
+    public void StartShooting()
+    {
+       InvokeRepeating("ShootFruit",startTime,Random.Range(minInterval,maxInterval));
+    }
+
+    // Update is called once per frame
+    void ShootFruit()
+    {
+         Instantiate(fruit,this.transform.position, this.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+	public float minForce;
+	public float maxForce;
+	public Rigidbody rb;
+	public GameObject fruitSlice;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+	   rb.AddForce(transform.forward * Random.Range(minForce,maxForce));
+    }
+	
+	
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "wall") {
+			
+			Instantiate(fruitSlice,this.transform.position,this.transform.rotation);
+			Destroy(this.gameObject);
+		}
+		
+	}
+	
+
+
+    void Update () {
+		
+		Destroy(this.gameObject,5f);
+		
+	}
+}
